5a869afa7c5fe6311055b5b39233dd2332e5d766
abiriadev <72962900+abiriadev@users.noreply.github.com>
2021-07-08 19:11:01 +0900

    feat: add SCSS and more
    * use sass to add `sass rollup-plugin-scss`
    * edit `rollup.config.js` to use scss
    * edit `<link rel='stylesheet' href='/build/assets.css'>` into `public/index.html`
    * add `svelte.config.js` file under root
    * edit `preprocess` in `svelte.config.js`
    * add `@rollup/plugin-alias`

---
+++ src/Number.svelte
@@ -0,0 +1,23 @@
+<script>
+    export let number
+
+</script>
+
+<div class="number">
+    {number}
+</div>
+
+<style lang="scss">
+    @import "./assets/scss/vars.scss";
+
+    .number {
+        width: 50px;
+        height: 50px;
+
+        border-radius: 20%;
+
+        background-color: $primary-color;
+
+        color: white;
+    }
+</style>
