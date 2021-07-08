537157be55d4b210d09666b537a5f2cb4328f428
abiriadev <72962900+abiriadev@users.noreply.github.com>
2021-07-08 19:27:05 +0900

    merge: merge conflict on App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,42 @@
 <script lang="ts">
-	export let name: string;
+    import Number from './Number.svelte'
+    import Grid from './Grid.svelte'
+
+    export let name: string
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+    <h1>Hello {name}!</h1>
+    <p>
+        Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn
+        how to build Svelte apps.
+    </p>
+
+    <Grid>
+        <Number number="7" />
+    </Grid>
 </main>
 
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+<style lang="scss">
+    @import './assets/scss/vars.scss';
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+    main {
+        text-align: center;
+        padding: 1em;
+        max-width: 240px;
+        margin: 0 auto;
+    }
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+    h1 {
+        color: $primary-color;
+        text-transform: uppercase;
+        font-size: 4em;
+        font-weight: 100;
+    }
+
+    @media (min-width: 640px) {
+        main {
+            max-width: none;
+        }
+    }
 </style>
---
+++ src/Grid.svelte
@@ -0,0 +1,19 @@
+<script>
+
+</script>
+
+<div class="numberbox-grid">
+    <div class="numberbox">
+
+    </div>
+</div>
+
+
+<style lang="scss">
+    .numberbox-grid {
+        display: grid;
+
+        grid-template-columns: repeat(1fr, 3);
+        grid-template-rows: repeat(1fr, 3);
+    }
+</style>
